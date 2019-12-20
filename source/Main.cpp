647bba05b235749d3762b52b9d67729387d63228
kuroneko96 <kaityougakusyuuyou@gmail.com>
2019-12-20 16:37:13 +0900

    [add]Mainのソースを追加しました

---
+++ source/Main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
+
+	MessageBox(NULL, TEXT("githab"), TEXT("test"), MB_OK);
+
+	return 0;
+}
