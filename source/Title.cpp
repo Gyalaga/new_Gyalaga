682e92e89c7a1e3f0325f1ad761bdaf40caf8c3f
Flamberg3983 <masaki1217jp@gmail.com>
2019-12-23 11:09:56 +0900

    パスの修正

--- source/Title.cpp
+++ source/Title.cpp
@@ -8,12 +8,12 @@ void Title_update() {
 }
 
 void Title_draw() {
-	static int title = LoadGraph("./res/img/Gyaraga.jpg"); static int namuco = LoadGraph("./res/img/NAMUCO.png");
-	static int player1 = LoadGraph("./res/img/PLAYER1.png"); static int player2 = LoadGraph("./res/img/PLAYER2.png");
-	static int score1 = LoadGraph("./res/img/1UP.png"); static int score2 = LoadGraph("./res/img/2UP.png");
-	static int hiscore = LoadGraph("./res/img/HISCORE.png");
+	static int title = LoadGraph("../res/img/Gyaraga.jpg"); static int namuco = LoadGraph("../res/img/NAMUCO.png");
+	static int player1 = LoadGraph("../res/img/PLAYER1.png"); static int player2 = LoadGraph("../res/img/PLAYER2.png");
+	static int score1 = LoadGraph("../res/img/1UP.png"); static int score2 = LoadGraph("../res/img/2UP.png");
+	static int hiscore = LoadGraph("../res/img/HISCORE.png");
 
-	DrawGraph(450, 500, title, TRUE);
-	DrawString(300, 300, "aaaaaaa",GetColor(255,255,255));
+	DrawGraph(350, 500, title, TRUE);
+	
 
 }
