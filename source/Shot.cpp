922a87a8e7fe56077aa176fa5ec1bc7753c33ff6
HI04 <rocksun410@gmail.com>
2019-12-23 10:50:19 +0900

    関数を作成

---
+++ source/Shot.cpp
@@ -0,0 +1,9 @@
+#include "DxLib.h"
+#include <Windows.h>
+
+
+// ƒvƒƒOƒ‰ƒ€‚Í WinMain ‚©‚çn‚Ü‚è‚Ü‚·
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	
+}
