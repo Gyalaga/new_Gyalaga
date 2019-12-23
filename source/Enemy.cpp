75c1c5bf8dbe309d68a663c55a3a8f4ac125493e
kuroneko96 <kaityougakusyuuyou@gmail.com>
2019-12-23 09:46:38 +0900

    [add]Enemy関連の追加

---
+++ source/Enemy.cpp
@@ -0,0 +1,6 @@
+#include "DxLib.h"
+#include "Enemy.h"
+
+void Enemy_All() {
+	DrawString(0, 0,"Enemy.cpp‚ð‹N“®’†",GetColor(255,255,255));
+}
---
+++ source/Enemy.h
@@ -0,0 +1,8 @@
+#ifndef _ENEMY_H_
+#define _ENEMY_H_
+
+//エネミーの全体管理
+void Enemy_All();
+
+
+#endif // !_ENEMY_H_
