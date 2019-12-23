f03a11bfe73d514aa2d9c393db4be61d755d31d9
kuroneko96 <kaityougakusyuuyou@gmail.com>
2019-12-23 10:47:06 +0900

    [add]ENEMYクラスの追加

--- source/Enemy.cpp
+++ source/Enemy.cpp
@@ -1,6 +1,13 @@
 #include "DxLib.h"
 #include "Enemy.h"
 
+ENEMY::ENEMY() {
+	Init();
+}
+
 void Enemy_All() {
 	DrawString(0, 0,"Enemy.cpp‚ð‹N“®’†",GetColor(255,255,255));
+}
+
+void ENEMY::Init() {
 }
--- source/Enemy.h
+++ source/Enemy.h
@@ -4,5 +4,20 @@
 //エネミーの全体管理
 void Enemy_All();
 
+//敵クラスの宣言
+class ENEMY {
+private:
+	int x;
+	int y;
+	int width;
+	int height;
+	int cx;
+	int cy;
+	bool onActive;
+public:
+	ENEMY();
+	void Init();
+};
+
 
 #endif // !_ENEMY_H_
