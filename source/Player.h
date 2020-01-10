186acce6b9ff9458e8840d1a7326caeea836226e
澤村遼 <ryo882514@gmail.com>
2020-01-10 11:59:23 +0900

    [add]Playerに爆発アニメーションを追加

--- source/Player.h
+++ source/Player.h
@@ -9,10 +9,12 @@ typedef struct
 	int y[16] = { 700,700,700,700,700,700,700,700,700,700,700,700,700,700,700,700 };
 	//  width:横幅, height:縦幅, center:中心
 	int width = 32, height = 32, center = width / 2;
-	//  rote:角度,image:画像
-	int rote = 0, image[2];
-	//  mode:  0_通常 ,1_デュアルモード ,2_捕縛されそう ,3_捕縛 ,4_破壊(非表示)
+	//  rote:角度,image:画像,hp:体力
+	int rote = 0, image[2],  hp = 3;
+	//  mode:  0_通常  ,2_捕縛されそう ,3_捕縛 ,4_破壊(非表示)
 	int mode = 0;
+	//デュアルモードフラグ
+	bool dualmode = false;
 	//表示フラグ
 	bool flg = true;
 }PLAYER;
@@ -20,13 +22,20 @@ typedef struct
 static PLAYER Player;            // Playerの構造体
 /***********************************************************************************/
 
-/*{ Player.cpp内だけで使う変数 }****************************************************/
+/*{ Player.cpp内だけで使う構造体 }****************************************************/
 typedef struct
 {
+	int i = 0, j = 0;                          // i:爆破エフェクトの速さ, j:表示
+	int check = 0;
+	int image[13];
 
-}Animation;
+	int check_left_x;                               // x座標を記録
+	int check_right_x;                              // x座標を記録
+	bool left_flg = false;
+	bool right_flg = false;
+}EXPLOSION; // 爆発用の構造体
 
-static Animation ani;            // Player_Animationの構造体
+static EXPLOSION explosion;            // 爆発用の構造体
 
 void Player_All();       //Playerの全体管理
 void Player_Init();      //Playerの初期化管理
