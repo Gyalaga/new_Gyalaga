d982e437326aff6007cdbe12babb06035963f681
HI04 <rocksun410@gmail.com>
2020-01-20 11:15:17 +0900

    [update]extern Playerを追加してプレイヤーの座標を受け取れるようにした。

--- source/Shot_Extern.h
+++ source/Shot_Extern.h
@@ -16,4 +16,22 @@ typedef struct {
 } BULLET;
 extern BULLET bullet;
 
+typedef struct
+{
+	int x[16] = { 400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400 };
+	int y[16] = { 700,700,700,700,700,700,700,700,700,700,700,700,700,700,700,700 };
+	//  width:横幅, height:縦幅, center:中心
+	int width = 32, height = 32, center = width / 2;
+	//  rote:角度,image:画像,hp:体力
+	int rote = 0, image[2], hp = 3;
+	//  mode:  0_通常  ,2_捕縛されそう ,3_捕縛 ,4_破壊(非表示)
+	int mode = 0;
+	//デュアルモードフラグ
+	bool dualmode = false;
+	//表示フラグ
+	bool flg = true;
+}PLAYER;
+
+extern PLAYER Player;
+
 #endif 
