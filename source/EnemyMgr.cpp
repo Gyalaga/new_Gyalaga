3d8f12762755ac95412ced9a6d4bb0dec91c2612
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-01-23 23:46:14 +0900

    [update]記述の変更

--- source/EnemyMgr.cpp
+++ source/EnemyMgr.cpp
@@ -22,9 +22,9 @@ void ENEMYMGR::Init() {
 	x3 = 364;
 	y3 = 0;
 
-	enemyAll		= 0;
-	enemyAllMove	= ENEMY_MOVERIGHT;
-	intervalCnt		= 0;
+	enemyAll = 0;
+	enemyAllMove = ENEMY_MOVERIGHT;
+	intervalCnt = 0;
 
 	//敵の画像を一時的にbaseGhに格納する
 	LoadDivGraph("./res/img/Galaga_OBJ_enemy.png", 50, 5, 10, 18, 18, baseGh);
@@ -45,47 +45,64 @@ void ENEMYMGR::Init() {
 		x += 18;
 		if (i == 9) {
 			y -= 18;
-			x =	310;
+			x = 310;
 		}
+	}
 
-		//ゴエイの実体を生成する
-		if (i < 16) {
-			goei[i] = new GOEI(x2, y2);
-			goei[i]->Load_Image(goeiGh);
-			x2 += 18;
-
-			if (i == 7) {
-				y2 -= 18;
-				x2 = 328;
-			}
+	//ゴエイの実体を生成する
+	for (int i = 0; i < 16; i++) {
+		goei[i] = new GOEI(x2, y2);
+		goei[i]->Load_Image(goeiGh);
+		x2 += 18;
+		if (i == 7) {
+			y2 -= 18;
+			x2 = 328;
 		}
+	}
 
-		if (i < 4) {
-			boss[i] = new BOSS(x3, y3);
-			boss[i]->Load_Image(bossGh);
-			x3 += 18;
-		}
+	//ボスギャラガの実体を追加する
+	for (int i = 0; i < 4; i++) {
+		boss[i] = new BOSS(x3, y3);
+		boss[i]->Load_Image(bossGh);
+		x3 += 18;
 	}
 }
 
 //終了処理
 void ENEMYMGR::Final() {
 
-	//確保していたメモリを解放する
+	//確保していたザコクラスのメモリを解放する
 	for (int i = 0; i < 20; i++) {
 		delete zako[i];
-		if (i < 16)delete goei[i];
-		if (i < 4)delete boss[i];
+	}
+
+	//確保していたゴエイクラスのメモリを解放する
+	for (int i = 0; i < 16; i++) {
+		delete goei[i];
+	}
+
+	//確保していたボスギャラガクラスのメモリを解放する
+	for (int i = 0; i < 4; i++) {
+		delete boss[i];
 	}
 }
 
 //更新処理
 void ENEMYMGR::Update() {
 
+	//ザコ
 	for (int i = 0; i < 20; i++) {
 		zako[i]->Update();
-		if (i < 16)goei[i]->Update();
-		if (i < 4)boss[i]->Update();
+	}
+
+	//ゴエイ
+	for (int i = 0; i < 16; i++) {
+		goei[i]->Update();
+	}
+
+	//ボスギャラガ
+	for (int i = 0; i < 4; i++) {
+		boss[i]->Update();
 	}
 
 	//間隔カウントが60を超えるとき初期化する
@@ -126,9 +143,18 @@ void ENEMYMGR::Update() {
 //描画処理
 void ENEMYMGR::Draw() {
 
+	//ザコ
 	for (int i = 0; i < 20; i++) {
 		zako[i]->Draw();
-		if (i < 16)goei[i]->Draw();
-		if (i < 4)boss[i]->Draw();
+	}
+
+	//ゴエイ
+	for (int i = 0; i < 16; i++) {
+		goei[i]->Draw();
+	}
+
+	//ボスギャラガ
+	for (int i = 0; i < 4; i++) {
+		boss[i]->Draw();
 	}
 }
