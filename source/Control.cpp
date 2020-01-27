2b902f14b5c46cb986270aa7607c76737d7f68dc
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-01-27 12:14:41 +0900

    [fix]当たり判定

--- source/Control.cpp
+++ source/Control.cpp
@@ -23,6 +23,7 @@ void CONTROL::GameControl() {
 	Score_Draw();
 
 	DrawFormatString(50, 520, GetColor(255, 255, 255), "%d,%d",px,py);
+	DrawFormatString(50, 540, GetColor(255, 255, 255), "%f,%f", ex[20], ey[20]);
 	DrawFormatString(50, 450, GetColor(255, 255, 255), "%d", flg);
 
 	if (flg == 1) {
@@ -32,13 +33,13 @@ void CONTROL::GameControl() {
 }
 
 void CONTROL::Hit_Judgment() {
-	/*
+
 	for (int i = 0; i < 40; i++) {
-		if (px + 32 >= ex[i] && px <= ex[i] + eWidth[i] && py + 32 >= ex[i] && py <= ey[i] + eHeight[i]) {
+		if ((double)px + pw >= ex[20] && (double)px <= ex[20] + eWidth[20] && (double)py + ph >= ex[20] && (double)py <= ey[20] + eHeight[20]) {
 			flg = 1;
 		}
 	}
-	*/
-	DrawFormatString(50, 620, GetColor(255, 255, 255), "%d,%d,%d,%d",sx, sy, sw, sh);
-	DrawFormatString(50, 520, GetColor(255, 255, 255), "%d,%d,%d,%d",px,py,pw,ph);
+
+	DrawFormatString(50, 620, GetColor(255, 255, 255), "%d,%d,%d,%d", sx, sy, sw, sh);
+	DrawFormatString(50, 520, GetColor(255, 255, 255), "%d,%d,%d,%d", px, py, pw, ph);
 }
